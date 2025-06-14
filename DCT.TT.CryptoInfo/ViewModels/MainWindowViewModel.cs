28713f0c500e3a1959630b2574ba86db918bb5b2
LavaRT <pavlo.legominecraft@gmail.com>
2025-06-14 12:44:11 +0300

    Add Property

---
+++ DCT.TT.CryptoInfo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using DCT.TT.CryptoInfo.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCT.TT.CryptoInfo.ViewModels
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        private string _Title;
+        public string Title
+        {
+            get => _Title;
+            set
+            {
+                Set(ref _Title, value);
+            }
+        }
+    }
+}
