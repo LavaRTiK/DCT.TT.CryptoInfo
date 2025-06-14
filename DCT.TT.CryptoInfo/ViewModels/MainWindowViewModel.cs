55364657e4ebe7196b2fc072158f37143f7a58b0
LavaRT <pavlo.legominecraft@gmail.com>
2025-06-14 17:53:16 +0300

    Add Command Exit

--- DCT.TT.CryptoInfo/ViewModels/MainWindowViewModel.cs
+++ DCT.TT.CryptoInfo/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,12 @@
-﻿using DCT.TT.CryptoInfo.ViewModels.Base;
+﻿using DCT.TT.CryptoInfo.Infrastructure.Commands;
+using DCT.TT.CryptoInfo.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace DCT.TT.CryptoInfo.ViewModels
 {
@@ -17,6 +20,23 @@ namespace DCT.TT.CryptoInfo.ViewModels
             {
                 Set(ref _Title, value);
             }
+        }
+        #region Commands
+        #region CloseApplicationCommand
+        public ICommand CloseApplicationCommand { get; }
+
+        private bool CanCloseApplicationCommandExecute(object p) => true;
+        private void OnCloseApllicationCommandExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        }
+        #endregion
+        #endregion
+        public MainWindowViewModel()
+        {
+            #region Commands
+            CloseApplicationCommand = new LambdaCommand(OnCloseApllicationCommandExecuted,CanCloseApplicationCommandExecute);
+            #endregion
         }
     }
 }
