c3e62eb64335d9d395ca9eab7b65e02b2d685a6e
LavaRT <pavlo.legominecraft@gmail.com>
2025-06-20 00:45:44 +0300

    PageService

---
+++ DCT.TT.CryptoInfo/Services/Registrator.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCT.TT.CryptoInfo.Services
+{
+    internal static class Registrator
+    {
+        public static IServiceCollection RegisterServices(this IServiceCollection services)
+        {
+            services.AddSingleton<PageService>();
+            //register services
+            return services;
+        }
+    }
+}
