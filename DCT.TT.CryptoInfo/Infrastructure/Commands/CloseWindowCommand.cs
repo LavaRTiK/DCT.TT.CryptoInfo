2644a71b0fa415d6482a9bd77a8d1ef06b82efa1
LavaRT <pavlo.legominecraft@gmail.com>
2025-06-20 00:44:39 +0300

    Add CloseAplication

---
+++ DCT.TT.CryptoInfo/Infrastructure/Commands/CloseWindowCommand.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Mime;
+using System.Windows;
+using DCT.TT.CryptoInfo.Infrastructure.Commands.Base;
+
+namespace DCT.TT.CryptoInfo.Infrastructure.Commands
+{
+    internal class CloseWindowCommand : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+        public override void Execute(object parameter)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
