fe9352c140d20b36164338ab5b07e3e8e9147d75
LavaRT <pavlo.legominecraft@gmail.com>
2025-06-24 19:13:43 +0300

    AddConvector

---
+++ DCT.TT.CryptoInfo/Infrastructure/Convertes/PercentBrushConvector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace DCT.TT.CryptoInfo.Infrastructure.Convertes
+{
+    internal class PercentBrushConvector : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double precent)
+            {
+                if (precent > 0) return Brushes.Green;
+                if (precent < 0) return Brushes.Red;
+            }
+
+            return Brushes.Green;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
