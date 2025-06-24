d0e1ddf64ca7221be315f8577382486757a6946c
LavaRT <pavlo.legominecraft@gmail.com>
2025-06-24 19:14:06 +0300

    Add convector NumberToShort

---
+++ DCT.TT.CryptoInfo/Infrastructure/Convertes/NumberToShort.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DCT.TT.CryptoInfo.Infrastructure.Convertes
+{
+    internal class NumberToShort : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return string.Empty;
+            if (value is double numberValue)
+            {
+                return ParseNumberShort(numberValue);
+            }
+            return value?.ToString() ?? string.Empty;
+        }
+
+        private string ParseNumberShort(double numberValue)
+        {
+            if (numberValue > 1_000_000_000_000)
+                return $"{numberValue / 1_000_000_000_000:F1}T";
+            if (numberValue > 1_000_000_000)
+                return $"{numberValue / 1_000_000_000:F1}B";
+            if (numberValue > 1_000_000)
+                return $"{numberValue / 1_000_000:F1}M";
+            if (numberValue > 1_000)
+                return $"{numberValue / 1000:F1}K";
+            return numberValue.ToString("F0");
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
